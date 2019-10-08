87b249da873dccd2cbc827dba9af36222f0063f5
Jeff Swisher <jefftswisher@gmail.com>
2019-10-08 16:48:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,14 @@ def countdown(num)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(num)
+  while num != 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1.second)
+  end
+  return "HAPPY NEW YEAR!"
+end
+
 
 
